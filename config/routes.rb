add0bdaa40fe08ecae9235615f87c4b39a40e6a5
ZacharyWeiner <zach@cloud.com>
2015-05-04 04:22:45 -0400

    enable users to place orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  
+
   devise_for :users
-  resources :listings
+  resources :listings do 
+    resources :orders
+  end
 
   get 'pages/about'
 
@@ -8,6 +12,7 @@ Rails.application.routes.draw do
   root 'listings#index'
 
   get 'seller' => 'listings#seller'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
